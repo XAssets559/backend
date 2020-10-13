7264f5d519064964eb6419a7beee31ecd7e96b5b
SamTao <1662146384@qq.com>
2020-10-13 19:33:21 +0800

    用类重写视图函数并增加success和error函数

---
+++ index/serializers.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/13 19:19
+# @Author  : Mr.Xia
+# @FileName: serializers.py
+# @Software: PyCharm
+
+from rest_framework import serializers
+from .models import *
+
---
+++ index/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/13 19:19
+# @Author  : Mr.Xia
+# @FileName: urls.py
+# @Software: PyCharm
+
+from django.urls import path,include
+
+urlpatterns = [
+
+]
